cf1cb9ce4e117281ce75b033d72affe823f5fdea
Shige <k024g0027@g.neec.ac.jp>
2025-04-16 14:32:44 +0900

    0-4履修

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Novice.h>
-#include <cassert>
+#include <cmath>
 
 const char kWindowTitle[] = "LC1A_10_シゲモリ_マサト_MT3";
 
@@ -13,50 +13,50 @@ struct Vector3 {
 	float z;
 };
 
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+Matrix4x4 MakeRotetaXMatrix(float radius) {
 	return {
-		{
-			{1, 0 ,0, 0},
-			{0, 1 ,0, 0},
-			{0, 0 ,1, 0},
-			{translate.x, translate.y, translate.z, 1}
-		}
+	1, 0, 0, 0 ,
+	0, std::cosf(radius), std::sinf(radius), 0 ,
+	0, -std::sinf(radius), std::cosf(radius), 0 ,
+	0, 0, 0, 1
 	};
 }
 
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
-	return{
-		{
-			{scale.x, 0 ,0, 0},
-			{0, scale.y ,0, 0},
-			{0, 0 ,scale.z, 0},
-			{0, 0 ,0, 1}
-		}
+Matrix4x4 MakeRotateYMatrix(float radius) {
+	return {
+		std::cosf(radius), 0, -std::sinf(radius), 0,
+		0, 1, 0, 0,
+		std::sinf(radius), 0, std::cosf(radius), 0,
+		0, 0, 0, 1
 	};
 }
 
-Vector3 TransForm(const Vector3& vector, const Matrix4x4& matrix) {
-	Vector3 ans;
-	ans.x = vector.x* matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + matrix.m[3][0];
-	ans.y = vector.x* matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + matrix.m[3][1];
-	ans.z = vector.x* matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + matrix.m[3][2];
-	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + matrix.m[3][3];
-	assert(w != 0.0f);
-	ans.x /= w;
-	ans.y /= w;
-	ans.z /= w;
-	return ans;
+Matrix4x4 MakeRotateZMatrix(float radius) {
+	return {
+		std::cosf(radius), std::sinf(radius), 0, 0,
+		-std::sinf(radius), std::cosf(radius), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	};
 }
 
-void VectorScreenPrintf(int x, int y, const Vector3& vec, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.2f	%.2f	%.2f	%s", vec.x, vec.y, vec.z, label);
+Matrix4x4 Multiply(const Matrix4x4& a, const Matrix4x4& b) {
+	Matrix4x4 result = {};
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			for (int k = 0; k < 4; ++k) {
+				result.m[i][j] += a.m[i][k] * b.m[k][j];
+			}
+		}
+	}
+	return result;
 }
 
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& mat, const char* label) {
 	Novice::ScreenPrintf(x, y, "%s", label);
-	for (int i = 1; i < 5; i++) {
+	for (int i = 1; i < 5; ++i) {
 		for (int j = 0; j < 4; j++) {
-			Novice::ScreenPrintf(x + j * 80, y + i * 20, "%.2f", mat.m[i - 1][j]);
+			Novice::ScreenPrintf(x + j * 80, y + i * 20, "%.2f", mat.m[i -1][j]);
 		}
 	}
 }
@@ -67,6 +67,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotetaXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -75,19 +81,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
 		Novice::BeginFrame();
-
-		Vector3 translate{ 4.1f, 2.6f, 0.8f };
-		Vector3 scale{ 1.5f, 5.2f, 7.3f };
-		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-		Vector3 point{ 2.3f, 3.8f, 1.4f };
-		Matrix4x4 transformMatrix = {
-			1.0f, 2.0f, 3.0f, 4.0f,
-			3.0f, 1.0f, 1.0f, 2.0f,
-			1.0f, 4.0f, 2.0f, 3.0f,
-			2.0f, 2.0f, 1.0f, 3.0f
-		};
-		Vector3 transformed = TransForm(point, transformMatrix);
 
 		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
@@ -105,9 +98,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, transformed, "transformed");
-		MatrixScreenPrintf(0, 20, translateMatrix, "translateMatrix");
-		MatrixScreenPrintf(0, 120, scaleMatrix, "scaleMatrix");
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, 100, rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, 200, rotateZMatrix, "rotateZMatrix");
+		MatrixScreenPrintf(0, 300, rotateXYZMatrix, "rotateXYZMatrix");
 
 		///
 		/// ↑描画処理ここまで
