91b62f249af42a0dd470976571886f4a8d8ef093
Shige <k024g0027@g.neec.ac.jp>
2025-04-16 14:46:38 +0900

    0-5履修

--- main.cpp
+++ main.cpp
@@ -13,6 +13,28 @@ struct Vector3 {
 	float z;
 };
 
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	return {
+		{
+			{1, 0 ,0, 0},
+			{0, 1 ,0, 0},
+			{0, 0 ,1, 0},
+			{translate.x, translate.y, translate.z, 1}
+		}
+	};
+}
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	return{
+		{
+			{scale.x, 0 ,0, 0},
+			{0, scale.y ,0, 0},
+			{0, 0 ,scale.z, 0},
+			{0, 0 ,0, 1}
+		}
+	};
+}
+
 Matrix4x4 MakeRotetaXMatrix(float radius) {
 	return {
 	1, 0, 0, 0 ,
@@ -52,11 +74,18 @@ Matrix4x4 Multiply(const Matrix4x4& a, const Matrix4x4& b) {
 	return result;
 }
 
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Matrix4x4 rotateMatrix = Multiply(MakeRotetaXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	return Multiply(Multiply(scaleMatrix, rotateMatrix), translateMatrix);
+}
+
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& mat, const char* label) {
 	Novice::ScreenPrintf(x, y, "%s", label);
 	for (int i = 1; i < 5; ++i) {
 		for (int j = 0; j < 4; j++) {
-			Novice::ScreenPrintf(x + j * 80, y + i * 20, "%.2f", mat.m[i -1][j]);
+			Novice::ScreenPrintf(x + j * 80, y + i * 20, "%.2f", mat.m[i - 1][j]);
 		}
 	}
 }
@@ -67,11 +96,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	Vector3 scale{ 1.2f, 0.79f, -2.1f };
 	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
-	Matrix4x4 rotateXMatrix = MakeRotetaXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+	Vector3 translate{ 2.7f, -4.15f, 1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -98,10 +126,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
-		MatrixScreenPrintf(0, 100, rotateYMatrix, "rotateYMatrix");
-		MatrixScreenPrintf(0, 200, rotateZMatrix, "rotateZMatrix");
-		MatrixScreenPrintf(0, 300, rotateXYZMatrix, "rotateXYZMatrix");
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 
 		///
 		/// ↑描画処理ここまで
