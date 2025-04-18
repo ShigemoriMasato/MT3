ac54554fec45fe7611a3abd44e3d757a0f3cf02c
Shige <k024g0027@g.neec.ac.jp>
2025-04-18 12:18:14 +0900

    1-0履修

--- main.cpp
+++ main.cpp
@@ -13,53 +13,8 @@ struct Vector3 {
 	float z;
 };
 
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
-	return {
-		{
-			{1, 0 ,0, 0},
-			{0, 1 ,0, 0},
-			{0, 0 ,1, 0},
-			{translate.x, translate.y, translate.z, 1}
-		}
-	};
-}
-
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
-	return{
-		{
-			{scale.x, 0 ,0, 0},
-			{0, scale.y ,0, 0},
-			{0, 0 ,scale.z, 0},
-			{0, 0 ,0, 1}
-		}
-	};
-}
-
-Matrix4x4 MakeRotetaXMatrix(float radius) {
-	return {
-	1, 0, 0, 0 ,
-	0, std::cosf(radius), std::sinf(radius), 0 ,
-	0, -std::sinf(radius), std::cosf(radius), 0 ,
-	0, 0, 0, 1
-	};
-}
-
-Matrix4x4 MakeRotateYMatrix(float radius) {
-	return {
-		std::cosf(radius), 0, -std::sinf(radius), 0,
-		0, 1, 0, 0,
-		std::sinf(radius), 0, std::cosf(radius), 0,
-		0, 0, 0, 1
-	};
-}
-
-Matrix4x4 MakeRotateZMatrix(float radius) {
-	return {
-		std::cosf(radius), std::sinf(radius), 0, 0,
-		-std::sinf(radius), std::cosf(radius), 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	};
+float cot(float radian) {
+	return 1.0f / tanf(radian);
 }
 
 Matrix4x4 Multiply(const Matrix4x4& a, const Matrix4x4& b) {
@@ -74,13 +29,6 @@ Matrix4x4 Multiply(const Matrix4x4& a, const Matrix4x4& b) {
 	return result;
 }
 
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
-	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-	Matrix4x4 rotateMatrix = Multiply(MakeRotetaXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
-	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-	return Multiply(Multiply(scaleMatrix, rotateMatrix), translateMatrix);
-}
-
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& mat, const char* label) {
 	Novice::ScreenPrintf(x, y, "%s", label);
 	for (int i = 1; i < 5; ++i) {
@@ -90,16 +38,42 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& mat, const char* label) {
 	}
 }
 
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	return {
+		cot(fovY / 2) / aspectRatio, 0, 0, 0,
+		0, cot(fovY / 2), 0, 0,
+		0, 0, farClip / (farClip - nearClip), 1,
+		0, 0, (-nearClip * farClip) / (farClip - nearClip), 0
+	};
+}
+
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	return {
+		2 / (right - left), 0, 0, 0,
+		0, 2 / (top - bottom), 0, 0,
+		0, 0, 1 / (farClip - nearClip), 0,
+		(left + right) / (left - right), (top + bottom) / (bottom - top), nearClip / (nearClip - farClip), 1
+	};
+}
+
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	return {
+		width / 2, 0, 0, 0,
+		0, -height / 2, 0, 0,
+		0, 0, maxDepth - minDepth, 0,
+		left + width / 2, top + height / 2, minDepth, 1
+	};
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 scale{ 1.2f, 0.79f, -2.1f };
-	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
-	Vector3 translate{ 2.7f, -4.15f, 1.57f };
-	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
+	Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+	Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -126,7 +100,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "OrthographicMatrix");
+		MatrixScreenPrintf(0, 100, perspectiveFovMatrix, "PerspectiveFovMatrix");
+		MatrixScreenPrintf(0, 200, viewportMatrix, "ViewportMatrix");
 
 		///
 		/// ↑描画処理ここまで
