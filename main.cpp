03f91c3dcfc0495c911aebe0d0911a3e4fd5d5f4
Shige <k024g0027@g.neec.ac.jp>
2025-05-16 11:04:20 +0900

    2-1

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ struct Matrix4x4 {
 	float m[4][4];
 };
 
+struct Vector2 {
+	float x;
+	float y;
+	float z;
+};
+
 struct Vector3 {
 	float x;
 	float y;
@@ -87,7 +93,7 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 
 }
 
-Matrix4x4 MakeRotetaXMatrix(float radius) {
+Matrix4x4 MakeRotateXMatrix(float radius) {
 
 	return {
 		1, 0, 0, 0,
@@ -132,7 +138,7 @@ Matrix4x4 Multiply(const Matrix4x4& a, const Matrix4x4& b) {
 
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
 	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-	Matrix4x4 rotateMatrix = Multiply(MakeRotetaXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
+	Matrix4x4 rotateMatrix = Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
 	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
 	return Multiply(Multiply(scaleMatrix, rotateMatrix), translateMatrix);
 }
@@ -352,7 +358,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 rotate{};
 	Vector3 translate{};
 	Vector3 cameraPosition{ 0.0f, 1.9f, -6.49f };
-	Vector3 CameraRotate{ 0.26f, 0.0f, 0.0f };
+	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
 	Vector3 CameraScale{ 1.0f, 1.0f, 1.0f };
 	Vector3 kLocalVertices[3] = {
 		{ 0.0f, 1.0f, 0.0f },
@@ -367,6 +373,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 project = Project(point - segment.origin, segment.diff);
 	Vector3 closestPoint = ClosestPoint(point, segment);
 
+	Sphere sphere{ { 0.0f, 0.0f, 0.0f }, 1.0f, 24 };
+	unsigned int color = 0xffffffff;
+	Sphere sphere2{ { 0.0f, 0.0f, 0.0f }, 0.5f, 24 };
+	Vector3 sphere2Position{ 1.0f, 0.0f, 0.0f };
+
+	int click = 0;
+	int preClick = 0;
+
+	Vector2 defaultMousePosition{};
+	bool a = false;
+
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -379,6 +396,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
+
+		// マウスの状態を取得
+		preClick = click;
+		click = Novice::IsPressMouse(0);
 
 		///
 		/// ↓更新処理ここから
@@ -400,14 +421,71 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			translate.x += kSpeed;
 		}
 
+		if (keys[DIK_Q]) {
+			translate.z += kSpeed;
+		}
+
+		if (keys[DIK_E]) {
+			translate.z -= kSpeed;
+		}
+
+		if (keys[DIK_LSHIFT] && click) {
+			int x;
+			int y;
+			if (!preClick || !preKeys[DIK_LSHIFT]) {
+				Novice::GetMousePosition(&x, &y);
+				defaultMousePosition = { (float)x, (float)y };
+			}
+			Novice::GetMousePosition(&x, &y);
+			Vector2 nowMousePosition = { (float)x, (float)y };
+
+			Matrix4x4 cameraRotateMatrix = Multiply(Multiply(MakeRotateXMatrix(cameraRotate.x), MakeRotateYMatrix(cameraRotate.y)), MakeRotateZMatrix(cameraRotate.z));
+
+			float buffer = (nowMousePosition.x - defaultMousePosition.x) * 0.01f;
+			cameraPosition = cameraPosition + Transform({ buffer, 0, 0 }, cameraRotateMatrix);
+			buffer = (nowMousePosition.y - defaultMousePosition.y) * 0.01f;
+			cameraPosition = cameraPosition - Transform({ 0, buffer, 0 }, cameraRotateMatrix);
+
+			defaultMousePosition = nowMousePosition;
+		}
+
+		if ((translate - sphere2Position).Length() < sphere.radius + sphere2.radius) {
+			color = 0xff0000ff;
+		} else {
+			color = 0xffffffff;
+		}
+
+		if (keys[DIK_LCONTROL] && click) {
+			int x;
+			int y;
+
+			if (a) {
+				a = false;
+			} else {
+				a = true;
+			}
+
+			if (!preClick || !preKeys[DIK_LCONTROL]) {
+				Novice::GetMousePosition(&x, &y);
+				defaultMousePosition = { (float)x, (float)y };
+			}
+			Novice::GetMousePosition(&x, &y);
+			Vector2 nowMousePosition = { (float)x, (float)y };
+
+			cameraRotate.y = cameraRotate.y + (nowMousePosition.x - defaultMousePosition.x) * 0.003f;
+			cameraRotate.x = cameraRotate.x + (nowMousePosition.y - defaultMousePosition.y) * 0.003f;
+
+			defaultMousePosition = nowMousePosition;
+		}
+
 		ImGui::Begin("Camera");
 		ImGui::SliderFloat3("Position", &cameraPosition.x, -10.0f, 10.0f);
-		ImGui::SliderFloat3("Rotate", &CameraRotate.x, -3.14f, 3.14f);
+		ImGui::SliderFloat3("Rotate", &cameraRotate.x, -3.14f, 3.14f);
 		ImGui::SliderFloat3("Scale", &CameraScale.x, 0.01f, 5.0f);
 		ImGui::End();
 
 		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f, 1.0f,1.0f }, rotate, translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix(CameraScale, CameraRotate, cameraPosition);
+		Matrix4x4 cameraMatrix = MakeAffineMatrix(CameraScale, cameraRotate, cameraPosition);
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
@@ -428,23 +506,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		Matrix4x4 normalWVPMatrix = Multiply(MakeAffineMatrix({ 1.0f, 1.0f,1.0f }, {}, {}), Multiply(viewMatrix, projectionMatrix));
 
-		Vector3 start = Transform(Transform(segment.origin, normalWVPMatrix), viewportMatrix);
-		Vector3 end = Transform(Transform(segment.diff, normalWVPMatrix), viewportMatrix);
-		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), 0x00ff00ff);
+		DrawGrid(Multiply(normalWVPMatrix, Multiply(viewMatrix, projectionMatrix)), viewportMatrix);
 
-		Sphere pointSphere{ point, 0.01f, 24 };
-		Sphere closestPointSphere{ closestPoint, 0.01f, 24 };
-		DrawSphere(pointSphere, normalWVPMatrix, viewportMatrix, 0xff0000ff);
-		DrawSphere(closestPointSphere, normalWVPMatrix, viewportMatrix, 0x000000ff);
+		DrawSphere(sphere2, Multiply(MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, {}, sphere2Position), Multiply(viewMatrix, projectionMatrix)), viewportMatrix, 0xffffffff);
 
-		DrawGrid(Multiply(MakeAffineMatrix({ 1.0f, 1.0f,1.0f }, {}, {}), Multiply(viewMatrix, projectionMatrix)), viewportMatrix);
-
-		//DrawSphere({ { 0.0f, 0.0f, 0.0f }, 1.0f, 24 }, Multiply(MakeAffineMatrix({ 1.0f, 1.0f,1.0f }, {}, {}), Multiply(viewMatrix, projectionMatrix)), viewportMatrix, 0xff);
-
-		/*if (screenVertices[0] * cross(screenVertices[1], screenVertices[2]) <= 0) {
-			Novice::DrawTriangle(int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[1].x), int(screenVertices[1].y),
-				int(screenVertices[2].x), int(screenVertices[2].y), RED, kFillModeSolid);
-		}*/
+		DrawSphere(sphere, Multiply(MakeAffineMatrix({1.0f, 1.0f, 1.0f}, {}, translate), Multiply(viewMatrix, projectionMatrix)), viewportMatrix, color);
 
 		///
 		/// ↑描画処理ここまで
